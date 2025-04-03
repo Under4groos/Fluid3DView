4038d37a42bd073c650a5906502d0964c370281e
Макс Титов <hhit934@gmail.com>
2025-04-03 15:42:24 +0300

    Добавьте файлы проекта.

---
+++ Fluid3DView/View/3DModels/View3DModel.cs
@@ -0,0 +1,36 @@
+﻿using HelixToolkit.Wpf;
+using System.Windows;
+using System.Windows.Media.Media3D;
+
+namespace Fluid3DView.View._3DModels
+{
+    public class View3DModel : ScreenSpaceVisual3D
+    {
+        public static readonly DependencyProperty CenterProperty = DependencyProperty.Register(
+            "Center", typeof(Point3D), typeof(CubeVisual3D), new UIPropertyMetadata(new Point3D(), GeometryChanged));
+
+        public Point3D Center
+        {
+            get
+            {
+                return (Point3D)this.GetValue(CenterProperty);
+            }
+
+            set
+            {
+                this.SetValue(CenterProperty, value);
+            }
+        }
+
+        protected override void UpdateGeometry()
+        {
+            
+            throw new NotImplementedException();
+        }
+
+        protected override bool UpdateTransforms()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
